a697361cff43c46a62b3750f919a2c37f0aea846
kutaiba98 <93405663+kutaiba98@users.noreply.github.com>
2022-02-11 18:16:22 +0200

    Add files via upload

---
+++ javascript/sign.js
@@ -0,0 +1,41 @@
+const userName = document.getElementById("userName")
+const userpass = document.getElementById("password")
+const submitBtn = document.getElementById("submitBtn")
+
+submitBtn.addEventListener("click", (e) =>{
+    e.preventDefault()
+    const name = userName.value 
+    const userPass = userpass.value 
+
+        localStorage.setItem("name",name)
+        localStorage.setItem("password",userPass)
+        location.reload()
+        if(localStorage.name && localStorage.password ){
+       window.location.href="../pages/profile.html";
+        }else{
+            window.alert("one feild or more is empty ,plaese try again ")
+        }
+    }) 
+
+    const userName2 = document.getElementById("userName2")
+    const email = document.getElementById("email2")
+    const userpass2 = document.getElementById("password2")
+    const submitBtn2 = document.getElementById("submitBtn2")
+
+    submitBtn2.addEventListener("click", (e) =>{
+        e.preventDefault()
+        const name = userName2.value 
+        const userPass = userpass2.value 
+        const email2 = email.value 
+
+            localStorage.setItem("name",name)
+            localStorage.setItem("email",email2)
+            localStorage.setItem("password",userPass)
+            location.reload()
+            if(localStorage.name && localStorage.password && localStorage.email){
+           window.location.href="../pages/profile.html";
+            }else{
+                window.alert("one feild or more is empty ,plaese try again ")
+            }
+        }) 
+    
